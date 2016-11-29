b1af0bd0acdb32a85076088b23a8b8d66b3676aa
Jacob Morton <mkoopajr@gmail.com>
2016-11-28 21:39:32 -0600

    Added rick roll

--- public/app/app.js
+++ public/app/app.js
@@ -1,37 +1,52 @@
 const hawtness = angular.module('hawtness', []);
 
-hawtness.controller('configController', ($scope, $http) => {
+hawtness.controller('configController', ($scope, $http, $sce) => {
 
     $scope.data = [];
     $scope.users = [];
     $scope.query = {per_page: 10};
+    $scope.rick = {roll: false};
+
+    var regex = /^(?!ron)/;
 
     $scope.getusers = () => {
 
         $scope.data = [];
         $scope.users = [];
 
-        $http({
-            method: 'GET',
-            url: 'https://api.github.com/search/users',
-            params: $scope.query
-        }).then((res) => {
-            $scope.data = res.data;
-            angular.forEach($scope.data.items, (value, key) => {
+        $scope.query.q = $scope.query.q.toLowerCase();
 
-                $http({
-                    method: 'GET',
-                    url: value.url
-                }).then((res) => {
-                    $scope.users.push(res);
-                }, (res) => {
-                    console.log(res);
+        if (!regex.test($scope.query.q)) {
+
+            $scope.rick.roll = true;
+            $scope.rick.url = $sce.trustAsResourceUrl('https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1');
+        } else {
+
+            $scope.rick.roll = false;
+            $scope.rick.url = '';
+
+            $http({
+                method: 'GET',
+                url: 'https://api.github.com/search/users',
+                params: $scope.query
+            }).then((res) => {
+                $scope.data = res.data;
+                angular.forEach($scope.data.items, (value, key) => {
+
+                    $http({
+                        method: 'GET',
+                        url: value.url
+                    }).then((res) => {
+                        $scope.users.push(res);
+                    }, (res) => {
+                        console.log(res);
+                    });
                 });
-            });
 
-            console.log($scope.users);
-        }, (res) => {
-            console.log(res);
-        });
+                console.log($scope.users);
+            }, (res) => {
+                console.log(res);
+            });
+        }
     }
 });
